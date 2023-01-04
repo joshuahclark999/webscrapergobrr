0e2c95508eb6208126484d4211889d7e4131af95
Joshuah Clark <joshuahclark999@gmail.com>
2023-01-04 14:22:36 -0500

    added words, removed redundant try catch block

--- webscraper.py
+++ webscraper.py
@@ -4,6 +4,7 @@ import requests
 import re
 from collections import Counter
 from prettytable import PrettyTable
+
 # Selenium imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,23 +13,17 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
   
 def extractSource(url):
-  try:
-    browser = webdriver.Firefox()
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.add_argument("--headless")
-    browser = webdriver.Firefox(options=fireFoxOptions)
-    browser.get(url)
-    browser.implicitly_wait(5)
-    body = browser.find_element(By.XPATH, "//body[1]")
-    return body.text
+  browser = webdriver.Firefox()
+  fireFoxOptions = webdriver.FirefoxOptions()
+  fireFoxOptions.add_argument("--headless")
+  browser = webdriver.Firefox(options=fireFoxOptions)
+  browser.get(url)
+  browser.implicitly_wait(5)
+  body = browser.find_element(By.XPATH, "//body[1]")
+  return body.text
     
-    browser.quit()
-  finally:
-    try:
-      browser.quit()
-    except:
-      pass
-      
+  browser.quit()
+   
 def getData(body, wordlist):
   htmlSplit = []
   goodWords = []

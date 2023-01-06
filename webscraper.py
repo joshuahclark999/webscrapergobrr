4a274782f856fe625b0bd30fd3a41da3ee2a4e81
Joshuah Clark <joshuahclark999@gmail.com>
2023-01-05 19:14:54 -0500

    touching on try catch driver bug

--- webscraper.py
+++ webscraper.py
@@ -13,21 +13,23 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
   
 def extractSource(url):
-  browser = webdriver.Firefox()
-  fireFoxOptions = webdriver.FirefoxOptions()
-  fireFoxOptions.add_argument("--headless")
-  browser = webdriver.Firefox(options=fireFoxOptions)
-  browser.get(url)
-  browser.implicitly_wait(5)
-  body = browser.find_element(By.XPATH, "//body[1]")
-  return body.text
-    
-  browser.quit()
+  try:
+    browser = webdriver.Firefox()
+    fireFoxOptions = webdriver.FirefoxOptions()
+    fireFoxOptions.add_argument("--headless")
+    browser = webdriver.Firefox(options=fireFoxOptions)
+    browser.get(url)
+    browser.implicitly_wait(5)
+    body = browser.find_element(By.XPATH, "//body[1]")
+    return body.text
+  finally:
+    browser.close()
+  
    
 def getData(body, wordlist):
   htmlSplit = []
   goodWords = []
-  wordCount = 0
+  
   table = PrettyTable()
 
   htmlSplit = body.split(" ")
@@ -36,7 +38,7 @@ def getData(body, wordlist):
     x = re.sub('[^+a-zA-Z\d\s:]', '', x)
     if x.lower() in wordlist:
       goodWords.append(x)
-      wordCount += 1 
+      
 
   goodWords = Counter(goodWords)
   goodWords = dict(sorted(goodWords.items(), key=lambda item: item[1]))
@@ -62,6 +64,7 @@ def main():
       wordList.append(word)  
 
   getData(extractSource(URLuser), wordList)
+  
   
 if __name__ == "__main__":
     main()
